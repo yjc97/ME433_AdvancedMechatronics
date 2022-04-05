86dd11fb2985617ff10ca7d7f44909d5afd45a64
yjc97 <jeff861205@gmail.com>
2022-04-05 18:28:50 -0500

    ME433

---
+++ HW1/StinkyTofu.md
@@ -0,0 +1,39 @@
+---
+layout: default
+---
+
+
+## Yujui Chen's Stinky Tofu Recipe:
+
+Stinky tofu is one of the most popular Taiwan cuisine in night market and it might not sound like the most appetising food, but don't be put off by the name! It's true that it does have an intense aroma and a corresponding depth of flavour, but it worth a try!
+
+### Ingredients: 
+
+- 14 oz of firm tofu, cubed 
+- 4 tbsp stinky fermented bean curd
+- 2 tsp potato starch or 1 tsp cornstarch 
+- 2 tsp oyster sauce
+- 2 finely chopped cloves of garlic 
+- 1 tbsp parsley
+- 1 tsp white vinegar
+- 1 tsp powdered fennel
+- 1 tsp vegetable oil
+
+
+### Instructions: 
+
+1. Take a large sealable container, such as a tupperware or plastic food storage container with a fitted lid. 
+2. In this, mix together the white vinegar, water and stinky fermented bean curd until you have a consistent liquid. 
+3. Place your tofu, chopped into chunks of around 1 in square, into the liquid, seal, and refrigerate for 24 to 48 hours. 
+   The longer you store it, the stronger the flavour will be. 
+4. Remove the tofu chunks from the liquid and drain them thoroughly. 
+5. Heat the oil in a wok, and fry your tofu chunks until the surface is golden. Place them to one side on a plate.
+6. Now fry the garlic until it becomes fragrant, and then add the oyster sauce, fennel powder and half a cup of water. 
+7. Bring it to the boil, then add the tofu chunks back into the wok and simmer for one minute. 
+8. Add chopped parsley and serve.
+9. Enjoy! Delicious Stinky Tofu!
+
+<!--
+Keep this link to return to the index
+-->
+[back to index](../)
---
+++ HW2/ME433_HW2.X/main.c
@@ -0,0 +1,85 @@
+#include <xc.h>          // processor SFR definitions
+#include <sys/attribs.h> // __ISR macro
+
+
+// DEVCFG0
+#pragma config DEBUG = OFF       // disable debugging
+#pragma config JTAGEN = OFF      // disable jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF         // disable flash write protect
+#pragma config BWP = OFF         // disable boot write protect
+#pragma config CP = OFF          // disable code protect
+
+// DEVCFG1
+#pragma config FNOSC = FRCPLL //  use fast frc oscillator with pll
+#pragma config FSOSCEN = OFF  // disable secondary oscillator
+#pragma config IESO = OFF     // disable switching clocks
+#pragma config POSCMOD = OFF   // primary osc disabled
+#pragma config OSCIOFNC = OFF // disable clock output
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD       // disable clock switch and FSCM
+#pragma config WDTPS = PS1048576    // use largest wdt value
+#pragma config WINDIS = OFF         // use non-window mode wdt
+#pragma config FWDTEN = OFF         // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz fast rc internal oscillator
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+
+// DEVCFG3
+#pragma config USERID = 0     // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF  // allow multiple reconfigurations
+
+int main() {
+
+  __builtin_disable_interrupts(); // disable interrupts while initializing
+                                  // things
+
+  // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+  __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+  // 0 data RAM access wait states
+  BMXCONbits.BMXWSDRM = 0x0;
+
+  // enable multi vector interrupts
+  INTCONbits.MVEC = 0x1;
+
+  // disable JTAG to get pins back
+  DDPCONbits.JTAGEN = 0;
+
+  // B4 is input
+  TRISBbits.TRISB4 = 1;
+
+  // A4 is output
+  TRISAbits.TRISA4 = 0;
+
+  // A4 is low
+  LATAbits.LATA4 = 0;
+
+  __builtin_enable_interrupts();
+
+  
+  while (1) {
+        // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
+        // remember the core timer runs at half the sysclk
+      
+    if (PORTBbits.RB4 == 0) {
+      LATAbits.LATA4 = 1;  
+      _CP0_SET_COUNT(0);
+      while(_CP0_GET_COUNT()<24000000/2){}
+      LATAbits.LATA4 = 0;
+      _CP0_SET_COUNT(0);
+      while(_CP0_GET_COUNT()<24000000/2){}
+      LATAbits.LATA4 = 1;
+      _CP0_SET_COUNT(0);
+      while(_CP0_GET_COUNT()<24000000/2){}
+      LATAbits.LATA4 = 0;
+      _CP0_SET_COUNT(0);
+      while(_CP0_GET_COUNT()<24000000/2){}
+      
+    }
+  }
+}
